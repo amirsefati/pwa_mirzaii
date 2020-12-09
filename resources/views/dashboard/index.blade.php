6bef7cd9bd4756ecfd0f2e01142b0dae7088a615
amir <sefati100@gmail.com>
2020-12-09 20:40:26 +0330

    fix width in index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,24 +14,25 @@
                         <p style="font-size: 18px;font-weight: bold;"> تاریخ اطلاعات : {{$data->d_j}}</p>
                     </div>
                 </div>
+                <div style="overflow-x: auto;">
                 <table class="table table-striped">
                     <thead>
                         <tr>
-                        <th scope="col" style="border:.4px solid gray"> 8 - 9:30</th>
-                        <th scope="col" style="border:.4px solid gray"> 9:30 - 11</th>
-                        <th scope="col" style="border:.4px solid gray"> 11 - 12:30</th>
-                        <th scope="col" style="border:.4px solid gray"> 12:30 - 14</th>
-                        <th scope="col" style="border:.4px solid gray"> 14 - 15:30</th>
-                        <th scope="col" style="border:.4px solid gray"> 15:30 - 17</th>
-                        <th scope="col" style="border:.4px solid gray"> 17 - 18:30</th>
-                        <th scope="col" style="border:.4px solid gray"> 18:30 - 20</th>
-                        <th scope="col" style="border:.4px solid gray"> 20 : 21:30</th>
+                        <th scope="col" style="border:.4px solid ;text-align:center"> 8 - 9:30</th>
+                        <th scope="col" style="border:.4px solid gray;text-align:center"> 9:30 - 11</th>
+                        <th scope="col" style="border:.4px solid gray;text-align:center"> 11 - 12:30</th>
+                        <th scope="col" style="border:.4px solid gray;text-align:center"> 12:30 - 14</th>
+                        <th scope="col" style="border:.4px solid gray;text-align:center"> 14 - 15:30</th>
+                        <th scope="col" style="border:.4px solid gray;text-align:center"> 15:30 - 17</th>
+                        <th scope="col" style="border:.4px solid gray;text-align:center"> 17 - 18:30</th>
+                        <th scope="col" style="border:.4px solid gray;text-align:center"> 18:30 - 20</th>
+                        <th scope="col" style="border:.4px solid gray;text-align:center"> 20 : 21:30</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                         @foreach(json_decode($data->data) as $d1)
-                            <td style="border:.4px solid gray">
+                            <td style="border:.4px solid gray;min-width:120px">
                                 @foreach($d1 as $d)
                                     <li style="font-size:10px;">{{App\Models\User::where('id',$d)->first()->name}}</li>
                                 @endforeach
@@ -42,6 +43,7 @@
                         
                     </tbody>
                 </table>
+                </div>
             </div>
         </div>
     </div>
