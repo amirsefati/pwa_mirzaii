4e84dec28d075364ec92e5f0fa80f4140f422e13
amir <sefati100@gmail.com>
2021-02-23 23:24:06 +0330

    i am confusing remove 2 commit

--- resources/views/dashboard/report_payment.blade.php
+++ resources/views/dashboard/report_payment.blade.php
@@ -16,7 +16,6 @@
                     <thead>
                     <tr>
                         <th>کاربر </th>
-                        <th>نوع کاربری </th>
                         <th>کد درگاه </th>
                         <th>قیمت </th>
                         <th>وضعیت </th>
@@ -38,46 +37,7 @@
                                    <span style="font-size: 9px;color:red">رزرو برای </span> <span style="font-size:7px">{{(App\Models\Report::where('etc2',$pay->id)->first()) ? (App\Models\Report::where('etc2',$pay->id)->first()->info) : ''}}</span>
                                 @endif
                             </td>
-                            <td>
-                            @if($pay->user_id == '2') <!--  یعنی مدیر بود-->
-                                <div >
-                                {{(App\Models\Report::where('etc2',$pay->id)->first()) ? ($etc3_t = App\Models\Report::where('etc2',$pay->id)->first()->etc3) : $etc3_t = '' }}
-                                </div>
-                                @if($etc3_t == 0)
-                                    <p>دانشجوی دانشکده فنی</p>
-                                @elseif($etc3_t == 1)
-                                    <p>دانشجوی غیر دانشکده فنی</p>
-                                @elseif($etc3_t == 2)
-                                    <p>اساتید و کارکنان دانشکده فنی</p>
-
-                                @elseif($etc3_t == 3)
-                                    <p> اساتید و کارکنان غیر دانشکده فنی </p>
-                                
-                                @elseif($etc3_t == 4)
-                                    <p>عادی</p>
-
-                                @endif
-                            @else
-                                <div style="display: none;">
-                                {{$user = App\Models\User::find($pay->user_id)->first()}}
-                                </div>
-                            @if($user->kind == 0)
-                                    <p>دانشجوی دانشکده فنی</p>
-                                @elseif($user->kind == 1)
-                                    <p>دانشجوی غیر دانشکده فنی</p>
-                                @elseif($user->kind == 2)
-                                    <p>اساتید و کارکنان دانشکده فنی</p>
-
-                                @elseif($user->kind == 3)
-                                    <p> اساتید و کارکنان غیر دانشکده فنی </p>
-                                
-                                @elseif($user->kind == 4)
-                                    <p>عادی</p>
-
-                                @endif
-                            @endif
-                
-                            </td>
+                            
                             <td>{{$pay->res_code}}</td>
 
                             <td>{{$pay->price}}</td>
